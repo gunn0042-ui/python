08716f826733a56e059a70aac8ab4db95cf2a81a
gunn0042-ui <gunn0042@gmail.com>
2026-07-05 07:42:13 +0545

    first push

---
+++ week2.py
@@ -0,0 +1,14 @@
+marks=int(input("Enter your marks: "))
+if marks>=90:
+    print("Distinction")
+elif marks>=75:
+    print("First division")
+elif marks>=60:
+    print("Second division")
+elif marks>=50:
+    print("Third division")
+elif marks>=35:
+    print("third division")
+else:
+    print("Fail")
+
